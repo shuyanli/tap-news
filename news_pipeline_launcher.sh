15b0ef417a9844b51172140d51395dcf83d49627
shuyanli <shuyanli@umail.ucsb.edu>
2018-07-25 23:20:28 -0700

    Update news_pipeline_launcher.sh

--- news_pipeline_launcher.sh
+++ news_pipeline_launcher.sh
@@ -2,14 +2,14 @@
 brew services start redis
 brew services start mongo
 
-#pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 
 cd web-server
 cd client
-#npm install
+npm install
 npm run build
 cd ../server
-# npm install
+npm install
 npm start &
 
 # Launch backend server
